aaec900ec296ee67fb0ec68fd53b754fb24fc8b5
Pedro Henrique Aguiar <pedrohenrykes@gmail.com>
2022-01-21 00:20:38 -0300

    Uncommenting TensorFlow requirement in FaceApi service

--- src/Services/FaceApi/index.js
+++ src/Services/FaceApi/index.js
@@ -1,4 +1,4 @@
-// require('@tensorflow/tfjs-node');
+require('@tensorflow/tfjs-node');
 const faceapi = require('@vladmandic/face-api');
 const { Canvas, Image, ImageData, loadImage } = require('canvas');
 
